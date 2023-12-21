f0568da097acbd7dfe0153b0b3ccb4b756016be1
edreenyusuf <154412074+edreenyusuf@users.noreply.github.com>
2023-12-21 14:59:08 +0800

    Update sales.generate.predict.py

--- sales.generate.predict.py
+++ sales.generate.predict.py
@@ -24,8 +24,8 @@ df = user_input_features()
 st.subheader('User Input Parameter')
 st.write(df)
 
-loaded_model = pickle.load(open("Sales-Model-ARNN-Project-V3.h5", "rb")
-#loaded_model = pickle.load(open("Sales-Model-ARNN-Project-V3.h5", "rb")
+#model = pickle.load(open("Sales-Model-ARNN-Project-V3.h5","rb)
+loaded_model = pickle.load(open("Sales-Model-ARNN-Project-V3.h5", "rb"))
 
 #prediction = loaded_model.predict(df)
 

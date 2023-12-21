65713603f10b283f0cc127cab478b3851fcd34c0
edreenyusuf <154412074+edreenyusuf@users.noreply.github.com>
2023-12-21 13:16:49 +0800

    sales.generate.predict.py

--- sales.generate.predict.py
+++ sales.generate.predict.py
@@ -8,10 +8,10 @@ st.write("This app predicts the **Sales** of Different Media!")
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    Tv = st.sidebar.slider('Tv', 0.7, 296.4, 100.0)
+    TV = st.sidebar.slider('TV', 0.7, 296.4, 100.0)
     Radio = st.sidebar.slider('Radio', 0.0, 49.6, 25.0)
     Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 60.0)
-    data = {'Tv': Tv,
+    data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
     features = pd.DataFrame(data, index=[0])

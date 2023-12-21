0dd4d4432f66eda94bfb11bf1d12d916b7dcab54
edreenyusuf <154412074+edreenyusuf@users.noreply.github.com>
2023-12-21 15:02:51 +0800

    Update sales.generate.predict.py

--- sales.generate.predict.py
+++ sales.generate.predict.py
@@ -17,6 +17,7 @@ def user_input_features():
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper,}
+    data = pd.DataFrame(data, index=[0])
     return data
 
 df = user_input_features()

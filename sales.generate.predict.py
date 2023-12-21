af1cdfa04d209942c873905032983bc65151f3e2
edreenyusuf <154412074+edreenyusuf@users.noreply.github.com>
2023-12-21 14:15:28 +0800

    sales.generate.predict.py

--- sales.generate.predict.py
+++ sales.generate.predict.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import keras
+import tensorflow
 
 st.write("# Advertising Sales Predictor App")
 st.write("This app predicts the **Sales** of Different Media!")
